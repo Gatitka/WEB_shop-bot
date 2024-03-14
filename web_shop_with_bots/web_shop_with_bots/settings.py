7679a418f61968431cd0e0c138889633ae66b22e
Nataly Kirillova <gatitka@yandex.ru>
2024-03-14 19:43:34 +0100

    settings fixed

--- web_shop_with_bots/web_shop_with_bots/settings.py
+++ web_shop_with_bots/web_shop_with_bots/settings.py
@@ -134,7 +134,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'project.context_processors.export_vars',
             ],
         },
     },
